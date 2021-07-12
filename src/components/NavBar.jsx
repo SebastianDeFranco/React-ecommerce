8f40ed8c21c75d5bb2a4cbaf39c2c75331a9c224
SebastianDeFranco <sebastianedefranco@gmail.com>
2021-07-12 16:08:48 -0300

    Sebastian De Franco

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Navbar,Nav } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function NavBar() {
+    return (
+        <div>
+        <Navbar bg="danger" variant="dark">
+        <Navbar.Brand href="#home">Pizzeria BA</Navbar.Brand>
+        <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Pizzas</Nav.Link>
+        <Nav.Link href="#pricing">Contacto</Nav.Link>
+        </Nav>
+        </Navbar>
+        </div>
+    )
+}
+
+export default NavBar
