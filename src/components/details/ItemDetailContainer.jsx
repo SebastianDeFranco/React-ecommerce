aae66b4dc605b51d6410f42ab2afc331ec21806c
SebastianDeFranco <sebastianedefranco@gmail.com>
2021-07-26 19:21:01 -0300

    Sebastian De Franco

---
+++ src/components/details/ItemDetailContainer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState, useEffect} from 'react'
+import ItemDetail from './ItemDetail'
+
+function ItemDetailContainer() {
+const [state, setstate] = useState()
+useEffect(() => {
+    effect
+    return () => {
+        cleanup
+    }
+}, [input])
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ItemDetailContainer
